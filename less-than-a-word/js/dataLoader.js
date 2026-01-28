7d223b6a8d474234e3368af4a02ce5b57cc989cb
sasahard <daisukesasahara4@gmail.com>
2026-01-28 23:25:23 +0900

    Remove export keyword from loadProfile and loadProgressData

--- less-than-a-word/js/dataLoader.js
+++ less-than-a-word/js/dataLoader.js
@@ -1,12 +1,12 @@
-// dataLoader.js
-export async function loadProfile(charId) {
+async function loadProfile(charId) {
   const response = await fetch(`data/characters/${charId}/profile.json`);
   return await response.json();
 }
 
-export async function loadProgressData(charId, progress) {
+async function loadProgressData(charId, progress) {
   const paddedProgress = progress.toString().padStart(4, '0');
   const fileName = `${charId}_${paddedProgress}.json`;
   const response = await fetch(`data/characters/${charId}/${fileName}`);
+  if (!response.ok) throw new Error('Progress not found');
   return await response.json();
 }
