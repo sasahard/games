00f6ab5e56030086e02ea00a2c8e72d89aee3031
sasahard <daisukesasahara4@gmail.com>
2026-01-28 23:29:29 +0900

    Refactor affection level functions into Utils object
    Refactored affection level calculation into Utils object and removed old functions.

--- less-than-a-word/js/utils.js
+++ less-than-a-word/js/utils.js
@@ -1,19 +1,8 @@
-// 好感度レベル判定
-function getAffectionLevel(affection, thresholds) {
-  for (let i = thresholds.length - 1; i >= 0; i--) {
-    if (affection >= thresholds[i]) return i;
+const Utils = {
+  calcAffectionLevel(affection, thresholds) {
+    for (let i = thresholds.length - 1; i >= 0; i--) {
+      if (affection >= thresholds[i]) return i;
+    }
+    return 0;
   }
-  return 0;
-}
-
-// LocalStorage操作
-function saveCharState(charId, progress, affection) {
-  localStorage.setItem(`char_${charId}_progress`, progress);
-  localStorage.setItem(`char_${charId}_affection`, affection);
-}
-
-function loadCharState(charId, initialProgress) {
-  const progress = parseInt(localStorage.getItem(`char_${charId}_progress`)) || initialProgress;
-  const affection = parseInt(localStorage.getItem(`char_${charId}_affection`)) || 0;
-  return { progress, affection };
-}
+};
